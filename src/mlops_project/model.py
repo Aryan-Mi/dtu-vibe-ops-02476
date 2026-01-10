3a356ebf770c801650f92dd092b4da1cb8a24a60
Catacity <wendyhui2371780044@gmail.com>
2026-01-10 16:45:52 +0100

    Drafted EfficientNet wrapper model
    Supports loading pretrained weights
    disabling batch norm layers
    and freezing backbone (don't think it makes too much sense, but we have to option to freeze it now if we want)

--- src/mlops_project/model.py
+++ src/mlops_project/model.py
@@ -1,7 +1,27 @@
 from torch import nn
 import torch
 import pytorch_lightning as pl
+from torchvision import models
+from torchvision.models import (
+    EfficientNet_B0_Weights, EfficientNet_B1_Weights, EfficientNet_B2_Weights,
+    EfficientNet_B3_Weights, EfficientNet_B4_Weights, EfficientNet_B5_Weights,
+    EfficientNet_B6_Weights, EfficientNet_B7_Weights,
+)
 
+# 0. Mapping of EfficientNet versions to their input sizes 
+# Credits to : https://www.kaggle.com/code/carlolepelaars/efficientnetb5-with-keras-aptos-2019 for the recommended size
+INPUT_SIZE = {
+    'b0': 224,
+    'b1': 240,
+    'b2': 260,
+    'b3': 300,
+    'b4': 380,
+    'b5': 456,
+    'b6': 528,
+    'b7': 600,
+}
+
+# 1. Baseline CNN Model
 class ConvBlock(nn.Module):
     """Building blocks for the baseline CNN model."""
     def __init__(self, in_ch: int, out_ch: int, use_bn : bool = True):
@@ -39,10 +59,8 @@ class BaselineCNN(pl.LightningModule):
 
         self.model = nn.Sequential(*layers)
 
-        # 3. Loss function
+        # 3. Loss function & learning rate
         self.criterion = nn.CrossEntropyLoss()
-
-        # 4. Learning rate
         self.lr = lr
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -77,7 +95,8 @@ class BaselineCNN(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
-    
+
+# 2. ResNet Model  
 class ResidualBlock(nn.Module):
     """ResNet Residual Block. Credits to : https://d2l.ai/chapter_convolutional-modern/resnet.html"""
     def __init__(self, in_ch: int, out_ch: int, stride: int=1, use_bn: bool=True):
@@ -144,16 +163,14 @@ class ResNet(pl.LightningModule):
 
         # 3. Final Classifier
         Layers.append(nn.AdaptiveAvgPool2d(1))  # Global average pooling. Will output (batch_size, channels, 1, 1)
-        Layers.append(nn.Dropout(dropout) if dropout and dropout > 0 else nn.Identity()) 
         Layers.append(nn.Flatten())
+        Layers.append(nn.Dropout(dropout) if dropout and dropout > 0 else nn.Identity()) 
         Layers.append(nn.Linear(output_channels[-1], num_classes))
 
         self.net = nn.Sequential(*Layers)
 
-        # 3. Loss function
+        # 3. Loss function & learning rate
         self.criterion = nn.CrossEntropyLoss()
-
-        # 4. Learning rate
         self.lr = lr
 
     def forward(self, x):
@@ -188,7 +205,87 @@ class ResNet(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
+
+# 3. EfficientNet Wrapper Model
+class EfficientNet(pl.LightningModule):
+    """EfficientNet model from torchvision. Based on this paper: https://arxiv.org/abs/1905.11946"""
+    def __init__(self, num_classes = 2, model_size: str = 'b5', lr: float = 1e-3, use_bn : bool = True, pretrained: bool = True, freeze_backbone: bool = False):
+        super().__init__()
+
+        # 0 . Define helper
+        def disable_bn_layers(module):
+            for name, child in module.named_children():
+                if isinstance(child, nn.BatchNorm2d):
+                    setattr(module, name, nn.Identity()) # Disabling BatchNorm by replacing them with Identity
+                     
+                disable_bn_layers(child) 
+
+        # 1. Load EfficientNet Backbone (and pretrained weights if specified)
+        efficientnet_configs = {
+            'b0': (models.efficientnet_b0, EfficientNet_B0_Weights),
+            'b1': (models.efficientnet_b1, EfficientNet_B1_Weights),
+            'b2': (models.efficientnet_b2, EfficientNet_B2_Weights),
+            'b3': (models.efficientnet_b3, EfficientNet_B3_Weights),
+            'b4': (models.efficientnet_b4, EfficientNet_B4_Weights),
+            'b5': (models.efficientnet_b5, EfficientNet_B5_Weights),
+            'b6': (models.efficientnet_b6, EfficientNet_B6_Weights),
+            'b7': (models.efficientnet_b7, EfficientNet_B7_Weights),
+        }
+        if model_size not in efficientnet_configs:
+            raise ValueError(f"Unsupported size: {model_size}")
+
+        self.model = efficientnet_configs[model_size][0](weights=efficientnet_configs[model_size][1].DEFAULT if pretrained else None) # Load pretrained weights if specified
+
+        # 2. Freeze backbone layers if needed
+        if freeze_backbone:
+            for param in self.model.features.parameters():
+                param.requires_grad = False
+
+        # 3. Modify the classifier to match num_classes
+        in_features = self.model.classifier[1].in_features
+        self.model.classifier[1] = nn.Linear(in_features, num_classes)
+
+        # 4. Remove BatchNorm layers if needed. 
+        if not use_bn:  
+            disable_bn_layers(self.model)
+
+        # 5. Loss function & learning rate
+        self.criterion = nn.CrossEntropyLoss()
+        self.lr = lr
+
+    def forward(self, x):
+        return self.model(x)
     
+    def training_step(self, batch: tuple, batch_idx):
+        data, target = batch
+        logits = self(data)
+
+        loss = self.criterion(logits, target)
+        pred = logits.argmax(dim=-1)
+        acc = (target == pred).float().mean()
+
+        # Logging to wandb
+        self.log('train_loss', loss)
+        self.log('train_acc', acc)
+
+        return loss
+    
+    def validation_step(self, batch: tuple) -> None:
+        data, target = batch
+        logits = self(data)
+
+        loss = self.criterion(logits, target)
+        pred = logits.argmax(dim=-1)
+        acc = (target == pred).float().mean()
+
+        # on_epoch=True to log epoch-level metrics
+        self.log('val_loss', loss, on_epoch=True)
+        self.log('val_acc', acc, on_epoch=True)
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
 if __name__ == "__main__":
     model = BaselineCNN()
     print(model)
