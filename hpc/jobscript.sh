abc99361365eb403d38db675d4e8ffe03043ae86
Yuen Yi Hui <wendyhui2371780044@gmail.com>
2026-01-13 15:12:02 +0100

    Created jobscript to run on hpc

---
+++ hpc/jobscript.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+### General options
+### –- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J mlops_job_32gb_baselineCNN_epochs:20 
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "select[gpu32gb]"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 1:00
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=5GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u your_email_address
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
+
+source /zhome/82/3/191611/MLOps/dtu-vibe-ops-02476/.venv/bin/activate
+cd /zhome/82/3/191611/MLOps/dtu-vibe-ops-02476
+
+python ./src/mlops_project/train.py --model-name BaselineCNN --data-path ./data/raw/ham10000 --max-epochs 20
