bd27e8bb144dfce5bfca7beaf394058c55a2da7a
trinitye <trinity_evans@sfu.ca>
2026-01-21 11:11:46 +0100

    test

--- src/mlops_project/train.py
+++ src/mlops_project/train.py
@@ -23,6 +23,7 @@ except ImportError:
     storage = None
 
 
+# upload_to_gcs
 def upload_to_gcs(local_path: str | Path, gcs_bucket: str, gcs_path: str) -> str | None:
     """Upload a file to GCS bucket.
 
